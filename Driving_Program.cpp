2d6cff49a0d5a11de027cde94a233e4416f699f9
SchuylerBridges <Schuyler Bridges>
2024-05-22 23:06:28 -0400

    Pushing just as a test

--- Driving_Program.cpp
+++ Driving_Program.cpp
@@ -1,2 +1,8 @@
 // this essentially takes in the sbus signal, modifies channel 1 and 4 for the steering+throttle, and shits out the new sbus
+#include "Driving.h"
 
+#include <HAL.h>
+#include <math.h>
+
+Driving::Driving(SBUS* x8r)
+    : x8r_(x8r), channels_{0.0}, 
