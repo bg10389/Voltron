709772edcf3ee19e1a6e3b52df35eb13e2969613
SchuylerBridges <Schuyler Bridges>
2024-05-23 00:48:05 -0400

    pls i am trying my best

--- Driving_Program.cpp
+++ Driving_Program.cpp
@@ -21,3 +21,63 @@ void Driving::loop() {
     }
 }
 
+bool Driving::get_timed_out() { return timed_out_; }
+
+bool Driving::read() {
+  bool success = x8r_->readCal(channels_, &failSafe_, &lostFrame_);
+  if (!success) return false;
+
+  if (!lostFrame_) {
+    reads_since_last_success_ = 0;
+    last_success_millis_ = hal_millis();
+  } else {
+    reads_since_last_success_++;
+  }
+
+  return true;
+}
+
+float Driving::get_throttle() {
+  float throttle = channels_[0];
+  
+  if (fabsf(throttle) < 0.01) {
+    // deadband
+    return 0.0;
+  }
+
+  return throttle;
+}
+
+float Driving::set_throttle(){
+    // if get_auto_mux() > -0.5
+        //throttle = 10
+}
+
+
+float Driving::get_steering() { 
+    return channels_[1]; 
+    }
+
+float Driving::set_Steering_loop(){
+    //if (cone >1 meter away){
+        //steering = -1.0
+    //if cone <1 meter away
+        //steering = -1.0
+    //else {
+        //steering = 0.0
+    //}
+    //}
+}
+
+bool Driving::get_deadman_held() { return (channels_[3] > -0.3); }
+
+bool Driving::get_armed() {
+  return (channels_[5]) > 0.5;  // we need to do this because the value we get
+                                // from the controller is exact and casting to
+                                // int doesnt round the normal way
+}
+
+bool Driving::get_auto_mux() { return (channels_[2]) > -0.5; }
+
+bool Driving::get_calibrate() { return (channels_[6]) > 0.5; }
+
